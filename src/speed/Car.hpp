89f67289a2e0b6a12173fedc10c48a9b54b8f074
BlackPhrase <BlackPhrase@users.noreply.gitlab.com>
2022-05-30 12:52:31 +0000

    Car max speed, acceleration & handling stats, tier & heat lvl

--- src/speed/Car.hpp
+++ src/speed/Car.hpp
@@ -11,6 +11,31 @@ public:
 	cCarEngine *GetEngine() const;
 	//cCarBodyPart *GetSpoiler() const;
 	cCarBodyPart *GetBodyPartByType(cCarBodyPart::Type aeType) const;
+	
+	void AddHeatLevel(float afValue){mfHeatLevel += afValue;}
+	float GetHeatLevel() const {return mfHeatLevel;}
+	
+	int GetTier() const {return mnTier;}
+	
+	// TODO: add base max speed, accel and handling values?
+	
+	void AddMaxSpeed(int anValue){mnMaxSpeed += anValue;}
+	int GetMaxSpeed() const {return mnMaxSpeed;}
+	
+	void AddAccel(int anValue){mnAccel += anValue;}
+	int GetAccel() const {return mnAccel;}
+	
+	void AddHandling(int anValue){mnHandling += anValue;}
+	int GetHandling() const {return mnHandling;}
+	
 private:
 	cCarBodyPart *mvBodyParts[cCarBodyPart::Type::SizeOf]{};
+	
+	float mfHeatLevel{0.0f};
+	
+	int mnTier{0};
+	
+	int mnMaxSpeed{0};
+	int mnAccel{0};
+	int mnHandling{0};
 };
